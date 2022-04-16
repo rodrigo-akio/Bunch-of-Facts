e59997f17077f0e503d5456783345398a251f687
Rodrigo <rodrigo.akio.qc@gmail.com>
2022-04-15 20:12:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bunch of Facts
+ 
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'facts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: const Facts(),
+      ),
+    );
+  }
+}
+
+class Facts extends StatefulWidget {
+  const Facts({Key? key}) : super(key: key);
+
+  @override
+  State<Facts> createState() => _FactsState();
+}
+
+class _FactsState extends State<Facts> {
+  // Number of array position for random facts
+  int factNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Center(
+        child: TextButton(
+            child: Text(
+              facts[factNum],
+              style: const TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            onPressed: () {
+              setState(() {
+                factNum = Random().nextInt(1005);
+              });
+            }),
+      ),
+    );
+  }
+}
