7718ec05dd7a81a69efd279ed0ab771bbd00201d
Rodrigo <dealmeida.qc@gmail.com>
2022-04-17 20:59:00 -0400

    Added Random Particles

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,74 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'facts.dart';
+import 'package:animated_background/animated_background.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  ParticleOptions particles = const ParticleOptions(
+    baseColor: Colors.white,
+    spawnOpacity: 0.0,
+    opacityChangeRate: 0.2,
+    minOpacity: 0.03,
+    maxOpacity: 0.1,
+    particleCount: 12,
+    spawnMaxRadius: 17.0,
+    spawnMinRadius: 10.0,
+    spawnMaxSpeed: 15.0,
+    spawnMinSpeed: 5.0,
+  );
+
+  int factNum = 1; // for position in array
+
+  static Color bgcolor = Colors.deepPurple.shade400;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
-        body: const Facts(),
-      ),
-    );
-  }
-}
-
-class Facts extends StatefulWidget {
-  const Facts({Key? key}) : super(key: key);
-
-  @override
-  State<Facts> createState() => _FactsState();
-}
-
-class _FactsState extends State<Facts> {
-  // Number of array position for random facts
-  int factNum = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Center(
-        child: TextButton(
-            style: ButtonStyle(
-              overlayColor: MaterialStateProperty.all(Colors.transparent), // no splash or highlights
-              minimumSize: MaterialStateProperty.all(const Size(double.infinity, double.infinity)), // max button size
-            ),
-
-            child: Text(
-              facts[factNum],
-              style: const TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20.0,
-                color: Colors.white,
-                fontWeight: FontWeight.normal,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            onPressed: () {
-              setState(() {
-                factNum = Random().nextInt(1005);
-              });
-            }),
+        backgroundColor: bgcolor,
+        body: AnimatedBackground(
+          vsync: this,
+          behaviour: RandomParticleBehaviour(options: particles),
+          child: Center(
+            child: TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(
+                      Colors.transparent), // no splash or highlights
+                  minimumSize: MaterialStateProperty.all(const Size(
+                      double.infinity, double.infinity)), // max button size
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    facts[factNum],
+                    style: const TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    factNum = Random().nextInt(1005);
+                  });
+                }),
+          ),
+        ),
       ),
     );
   }
