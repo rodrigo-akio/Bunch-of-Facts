8996469d8f17761e9b3f6d0b5605bb92166888b9
Rodrigo <dealmeida.qc@gmail.com>
2022-04-19 14:21:13 -0400

    Built web
    Branch: main
    Commit: ce74bf34f3ffbf8432bb9fe17dcb77c98ff8f1fa (dirty)
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "06da581b77f904f069a1f2961aac8083",
-"index.html": "a3b36196ac6a1a473a45fbbddaeb6fb4",
-"/": "a3b36196ac6a1a473a45fbbddaeb6fb4",
-"main.dart.js": "b7c74207ac34997ec290689cc7eb1692",
+"index.html": "d701324231888f0347278890a0b23a65",
+"/": "d701324231888f0347278890a0b23a65",
+"main.dart.js": "e5d8c262be31bf175d03b05aa6a558be",
+"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,10 +15,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "cfd436216a3e2c88f08deac8842e14d7",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "6931ba913361303dd3a2b1bc91dfae2f",
+"assets/NOTICES": "9940387f673d99d65968f9320e28275b",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
@@ -27,8 +28,7 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
 "assets/NOTICES",
 "assets/AssetManifest.json",
